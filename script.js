9b973d7df6122930313c56638e4cfa2e654a239f
Zack Honey <zhoney466@gmail.com>
2024-02-01 21:34:02 -0800

    js file started

---
+++ script.js
@@ -0,0 +1,14 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen')
+
+function buttonClick(value) {
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else{
+        handeNumber(value);
+    }
+    screen.innerText = buffer;
+}
